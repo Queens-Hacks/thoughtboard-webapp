9d73d8f2598945fa45c85a864f899b5ab48f4e5a
Riley Shaw <rileyjshaw@gmail.com>
2014-05-31 21:09:14 -0400

    Start work on cards

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
-var geoLocation = require('geo');
-var localStorage = require('localStorage');
-var utils = require('utils');
+var cards = require('./cards');
+var DOM = require('./DOM');
+var geoLocation = require('./geo');
+var score = require('./score');
+var utils = require('./utils');
 
-if (!localStorage.isSupported) {
+if (!score.isSupported) {
   utils.abort('Get a better browser you dummy');
 }
+
+// alert('Welcome! Once you\'ve looked through a few submissions you can submit a thought of your own.');
+
