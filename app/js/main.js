d5914783a076cfcc30484a2e2c3ae58d91b260db
Riley Shaw <rileyjshaw@gmail.com>
2014-06-01 11:05:36 -0400

    Remove some crusty modules

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 var buttons = require('./buttons');
 var cards = require('./cards');
 var DOM = require('./DOM');
-var geo = require('./geo');
 var score = require('./score');
 var server = require('./server');
 var touch = require('./touch');
