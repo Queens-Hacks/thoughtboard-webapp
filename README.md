3f5b3e5dcac9e5602428940eebc32fcd3a4736c1
Riley Shaw <rileyjshaw@gmail.com>
2014-06-01 02:35:50 -0400

    Wipe gh-pages branch

--- README.md
+++
@@ -1,4 +0,0 @@
-the-best-idea-webapp
-=====================
-
-evidently
--- app/js/DOM.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  score: document.querySelector('.score'),
-  heart: document.querySelector('.heart'),
-  next: document.querySelector('.next'),
-  newSubmission: document.querySelector('.newSubmission'),
-  cardList: document.querySelector('.cardList')
-};
