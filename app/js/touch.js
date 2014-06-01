b1f74f5db9217517b5600ff363282f1c9e115c85
herlifeinpixels <hannah@uken.com>
2014-06-01 05:57:57 -0400

    touch.js functions whoa

--- app/js/touch.js
+++ app/js/touch.js
@@ -1,13 +1,44 @@
 var DOM = require('./DOM');
 
+var tappedHeart = false;
+var tappedNext = false;
+var dragRight = false;
+
+var hasTouch = {
+  'heart' : false,
+  'swipe': false,
+  'navigate' : false
+};
+
 var tapHeart = Hammer(DOM.heart).on("tap", function(event) {
   console.log('hearted!');
+  if (!tappedHeart) {
+    hasTouch.heart = true;
+  }
 });
 
-var tapNext = Hammer(DOM.cardList).on("tap", function(event) {
-  console.log('hearted!');
+var dragRight = Hammer(DOM.cardList).on("dragright", function(event) {
+  console.log('dragged Right!');
+  if (!dragRight) {
+    hasTouch.swipe = true;
+  }
 });
 
-var swipeRight = Hammer(DOM.next).on("swiperight", function(event) {
-  console.log('tapped right!');
+var tapNext = Hammer(DOM.next).on("tap", function(event) {
+  console.log('tapped next!');
+  if (!tappedNext) {
+    hasTouch.navigate = true;
+  }
 });
+
+function resetStates() {
+  _.map(hasTouch, function(touchItem) {
+    touchItem = false;
+  });
+  console.log("reset touch: " + hasTouch.heart);
+};
+
+module.exports = {
+  hasTouch: hasTouch,
+  reset: resetStates
+};
